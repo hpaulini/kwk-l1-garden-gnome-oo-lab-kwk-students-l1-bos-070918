57c3da66f7adcc2c3f15213b4b54780bda11ec56
hpaulini <helenpaulini@gmail.com>
2018-07-13 15:00:19 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -6,8 +6,9 @@ class GardenGnome
   
   attr_accessor :name, :age, :gluten_allergy
   
-  def initialize (personality = "evil", hat_color)
+  def initialize (personality = "evil", hat_color = "red")
     @personality = personality
+    @hat_color
   end
   
 end
