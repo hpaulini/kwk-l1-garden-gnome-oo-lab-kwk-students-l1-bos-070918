3606630366b29eb0a04e5c970990e7ec9711a1a2
hpaulini <helenpaulini@gmail.com>
2018-07-13 15:04:19 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -20,6 +20,6 @@ class GardenGnome
   end
   
   def introduce_self
-    "Hello humans, my name is "
+    "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
   end
 end
